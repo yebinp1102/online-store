131fa84abd184c9d1a7a628522b4f70eac4ed945
yebin Park <yebinp1102@naver.com>
2022-04-10 12:14:48 +0900

    create product detail images component

--- client/src/components/views/DetailProductPage/Detail.js
+++ client/src/components/views/DetailProductPage/Detail.js
@@ -1,17 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
+import ProductImage from './Section/ProductImage'
+import ProductInfo from './Section/ProductInfo'
 
 const Detail = () => {
-
   const {productId} = useParams();
+  const [product, setProduct] = useState({})
 
   useEffect(()=>{
     axios.get(`/api/product/products_by_id?id=${productId}&type=single`)
       .then(res=>{
         if(res.data.success){
           console.log(res.data)
+          setProduct(res.data.product[0])
         }else{
           alert('상품 상세 정보를 가져오는 데 실패 했습니다.')
         }
@@ -19,8 +22,23 @@ const Detail = () => {
   },[])
 
   return (
-    <div>Detail</div>
+    <DetailWrap>
+      <div className='container'>
+
+        <h1>{product.title}</h1>
+
+        <div className='grid-2 pd-2'>
+          <ProductImage detail={product} />
+          <ProductInfo />
+        </div>
+
+      </div>
+    </DetailWrap>
   )
 }
 
 export default Detail
+
+const DetailWrap = styled.section`
+
+`;
